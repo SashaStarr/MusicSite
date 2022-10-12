065ba4721fbb37996ca3d94dec5bf9ec80526696
SashaStarr <sashastar1234123@gmail.com>
2022-10-13 00:29:33 +0300

    ggggg

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,12 +1,24 @@
 import './MainPage.css';
-import Logo from '../../resourses/img/Logo.png';
 import MainText from './mainText/MainText';
 import MainFooter from './mainFooter/MainFooter';
 
 const MainPage = () => {
     return (
         <header className="header">
-            <span><img src={Logo} alt='logo' /></span>
+            <div className='main-logo'>
+                <div className="logo-animate">
+                    <div className='logo-animate_stick'></div>
+                    <div className='logo-animate_stick'></div>
+                    <div className='logo-animate_stick'></div>
+                    <div className='logo-animate_stick'></div>
+                    <div className='logo-animate_stick'></div>
+                    <div className='logo-animate_stick'></div>
+                    <div className='logo-animate_stick'></div>
+                    <div className='logo-animate_stick'></div>
+                </div>
+                <span className='main-logo_title'>FU<span>II</span> MUS<span>I</span>C</span>
+                <span className='main-logo_undertext'>МУЗЫКАЛЬНЫЙ ЛЕЙБЛ</span>
+            </div>
             <MainText />
             <div>
                 <MainFooter />
